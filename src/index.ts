794c9421040b63e48d985591108fc5f1dd73b008
Yuriy Kuzin <yuriy.kuzin@gmail.com>
2018-10-30 17:07:49 +0200

    setup our groups of imports

--- src/index.ts
+++ src/index.ts
@@ -1,341 +1,68 @@
-import {IStyleAPI, IStyleItem} from 'import-sort-style';
+import { IStyleAPI, IStyleItem } from 'import-sort-style';
 
 export default function(styleApi: IStyleAPI): Array<IStyleItem> {
   const {
     and,
-    hasDefaultMember,
-    hasNamedMembers,
-    hasNamespaceMember,
-    hasNoMember,
-    hasOnlyDefaultMember,
-    hasOnlyNamedMembers,
-    hasOnlyNamespaceMember,
-    isAbsoluteModule,
-    isRelativeModule,
     member,
     name,
     not,
-    startsWithAlphanumeric,
-    startsWithLowerCase,
-    startsWithUpperCase,
     unicode,
   } = styleApi;
 
+  const isAngularModule = (imported) => Boolean(imported.moduleName.match(/^@angular\//));
+
+  const isLocalModule = (imported) => (
+    [
+      '@app',
+      './',
+      '../',
+      '~/'
+    ]
+      .some((mask: string) => imported.moduleName.startsWith(mask))
+  );
+
+  const isConfig = (imported) => (
+    [
+      '@env',
+      '/env',
+      '/environment',
+      '/config',
+      '-routes',
+      '/routes'
+    ].some((mask: string) => imported.moduleName.endsWith(mask))
+  );
+
   return [
-    // import "foo"
-    {match: and(hasNoMember, isAbsoluteModule)},
-    {separator: true},
-
-    // import "./foo"
-    {match: and(hasNoMember, isRelativeModule)},
-    {separator: true},
-
-    // import * as _ from "bar";
+    // import {...} "@angular/...";
     {
-      match: and(
-        hasOnlyNamespaceMember,
-        isAbsoluteModule,
-        not(member(startsWithAlphanumeric)),
-      ),
-      sort: member(unicode),
-    },
-    // import * as Foo from "bar";
-    {
-      match: and(
-        hasOnlyNamespaceMember,
-        isAbsoluteModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-    },
-    // import * as foo from "bar";
-    {
-      match: and(
-        hasOnlyNamespaceMember,
-        isAbsoluteModule,
-        member(startsWithLowerCase),
-      ),
-      sort: member(unicode),
-    },
-
-    // import _, * as bar from "baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamespaceMember,
-        isAbsoluteModule,
-        not(member(startsWithAlphanumeric)),
-      ),
-      sort: member(unicode),
-    },
-    // import Foo, * as bar from "baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamespaceMember,
-        isAbsoluteModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-    },
-    // import foo, * as bar from "baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamespaceMember,
-        isAbsoluteModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-    },
-
-    // import _ from "bar";
-    {
-      match: and(
-        hasOnlyDefaultMember,
-        isAbsoluteModule,
-        not(member(startsWithAlphanumeric)),
-      ),
-      sort: member(unicode),
-    },
-    // import Foo from "bar";
-    {
-      match: and(
-        hasOnlyDefaultMember,
-        isAbsoluteModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-    },
-    // import foo from "bar";
-    {
-      match: and(
-        hasOnlyDefaultMember,
-        isAbsoluteModule,
-        member(startsWithLowerCase),
-      ),
-      sort: member(unicode),
-    },
-
-    // import _, {bar, …} from "baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamedMembers,
-        isAbsoluteModule,
-        not(member(startsWithAlphanumeric)),
-      ),
+      match: isAngularModule,
       sort: member(unicode),
       sortNamedMembers: name(unicode),
     },
-    // import Foo, {bar, …} from "baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamedMembers,
-        isAbsoluteModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-      sortNamedMembers: name(unicode),
-    },
-    // import foo, {bar, …} from "baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamedMembers,
-        isAbsoluteModule,
-        member(startsWithLowerCase),
-      ),
-      sort: member(unicode),
-      sortNamedMembers: name(unicode),
-    },
+    { separator: true },
 
-    // import {_, bar, …} from "baz";
+    // third party and something not expected
     {
-      match: and(
-        hasOnlyNamedMembers,
-        isAbsoluteModule,
-        not(member(startsWithAlphanumeric)),
-      ),
+      match: and(not(isAngularModule), not(isLocalModule)),
       sort: member(unicode),
       sortNamedMembers: name(unicode),
     },
-    // import {Foo, bar, …} from "baz";
-    {
-      match: and(
-        hasOnlyNamedMembers,
-        isAbsoluteModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-      sortNamedMembers: name(unicode),
-    },
-    // import {foo, bar, …} from "baz";
-    {
-      match: and(
-        hasOnlyNamedMembers,
-        isAbsoluteModule,
-        member(startsWithLowerCase),
-      ),
-      sort: member(unicode),
-      sortNamedMembers: name(unicode),
-    },
+    { separator: true },
 
-    {separator: true},
-
-    // import * as _ from "./bar";
+    // env, config, routes
     {
-      match: and(
-        hasOnlyNamespaceMember,
-        isRelativeModule,
-        not(member(startsWithAlphanumeric)),
-      ),
-      sort: member(unicode),
-    },
-    // import * as Foo from "./bar";
-    {
-      match: and(
-        hasOnlyNamespaceMember,
-        isRelativeModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-    },
-    // import * as foo from "./bar";
-    {
-      match: and(
-        hasOnlyNamespaceMember,
-        isRelativeModule,
-        member(startsWithLowerCase),
-      ),
-      sort: member(unicode),
-    },
-
-    // import _, * as bar from "./baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamespaceMember,
-        isRelativeModule,
-        not(member(startsWithAlphanumeric)),
-      ),
-      sort: member(unicode),
-    },
-    // import Foo, * as bar from "./baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamespaceMember,
-        isRelativeModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-    },
-    // import foo, * as bar from "./baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamespaceMember,
-        isRelativeModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-    },
-
-    // import _ from "./bar";
-    {
-      match: and(
-        hasOnlyDefaultMember,
-        isRelativeModule,
-        not(member(startsWithAlphanumeric)),
-      ),
-      sort: member(unicode),
-    },
-    // import Foo from "./bar";
-    {
-      match: and(
-        hasOnlyDefaultMember,
-        isRelativeModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-    },
-    // import foo from "./bar";
-    {
-      match: and(
-        hasOnlyDefaultMember,
-        isRelativeModule,
-        member(startsWithLowerCase),
-      ),
-      sort: member(unicode),
-    },
-
-    // import _, {bar, …} from "./baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamedMembers,
-        isRelativeModule,
-        not(member(startsWithAlphanumeric)),
-      ),
+      match: and(isConfig, isLocalModule),
       sort: member(unicode),
       sortNamedMembers: name(unicode),
     },
-    // import Foo, {bar, …} from "./baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamedMembers,
-        isRelativeModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-      sortNamedMembers: name(unicode),
-    },
-    // import foo, {bar, …} from "./baz";
-    {
-      match: and(
-        hasDefaultMember,
-        hasNamedMembers,
-        isRelativeModule,
-        member(startsWithLowerCase),
-      ),
-      sort: member(unicode),
-      sortNamedMembers: name(unicode),
-    },
+    { separator: true },
 
-    // import {_, bar, …} from "./baz";
+    // local imports
     {
-      match: and(
-        hasOnlyNamedMembers,
-        isRelativeModule,
-        not(member(startsWithAlphanumeric)),
-      ),
+      match: isLocalModule,
       sort: member(unicode),
       sortNamedMembers: name(unicode),
     },
-    // import {Foo, bar, …} from "./baz";
-    {
-      match: and(
-        hasOnlyNamedMembers,
-        isRelativeModule,
-        member(startsWithUpperCase),
-      ),
-      sort: member(unicode),
-      sortNamedMembers: name(unicode),
-    },
-    // import {foo, bar, …} from "./baz";
-    {
-      match: and(
-        hasOnlyNamedMembers,
-        isRelativeModule,
-        member(startsWithLowerCase),
-      ),
-      sort: member(unicode),
-      sortNamedMembers: name(unicode),
-    },
-
-    {separator: true},
+    { separator: true }
   ];
 }
